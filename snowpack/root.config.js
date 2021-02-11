8a1eab4a7b3a799c4ab4395f8b4522238a368b40
John Livingston <jmlivingston@gmail.com>
2021-02-10 21:11:23 -0800

    Change start to root

--- snowpack/root.config.js
+++ snowpack/root.config.js
@@ -3,5 +3,5 @@ module.exports = {
   buildOptions: {
     baseUrl: '/snowpack-react-doc-gen',
   },
-  routes: [{ match: 'routes', src: '.*', dest: '/snowpack/start-index.html' }],
+  routes: [{ match: 'routes', src: '.*', dest: '/snowpack/root-index.html' }],
 }
--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import Button from './components/Button/Button'
 
 function App() {
-  return <Button onClick={console.log}>Foo</Button>
+  return (
+    <>
+      <h1>App</h1>
+      <Button onClick={console.log}>Foo</Button>
+    </>
+  )
 }
 
 export default App
