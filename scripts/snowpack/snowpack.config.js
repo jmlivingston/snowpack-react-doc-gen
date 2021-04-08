c241cc5c5ad024cc2557d9ff0bd6c50700acf2a8
John Livingston <jmlivingston@gmail.com>
2021-04-08 10:50:19 -0700

    Add refresh

--- scripts/snowpack/snowpack.config.js
+++ scripts/snowpack/snowpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     sourcemap: true,
   },
   exclude: ['**/node_modules/**/*', '**/scripts/**/*.mjs'],
+  plugins: ['@snowpack/plugin-react-refresh'],
   routes: [
     {
       match: 'routes',
